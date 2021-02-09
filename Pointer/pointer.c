6ef7d03f97231b4e87dcbe42ee2b591d6a75a451
isyankarcoder <gencvolkan@gmail.com>
2021-02-09 13:32:45 +0300

    pointer

--- Pointer/pointer.c
+++ Pointer/pointer.c
@@ -4,4 +4,20 @@ int main(){
     c = 22;
     printf("Adress of c: %p\n", &c);
     printf("Value of c: %d\n\n ", c);
+
+    pc = &c;
+    printf("Address of c: %p\n", pc);
+    printf("Value of pc %d\n\n", *pc);
+
+    c = 11;
+    printf("Address of c: %p\n", pc);
+    printf("Value of pc %d\n\n", *pc);
+
+    *pc = c;
+     printf("Address 2 of c: %p\n", pc);
+    printf("Value 2 of pc %d\n\n", *pc);
+
+    *pc = 55;
+     printf("Adress of c: %p\n", &c);
+    printf("Value of c: %d\n\n ", c);
 }
