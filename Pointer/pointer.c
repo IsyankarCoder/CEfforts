bc84cb0c6962d8c7dbf5181ac26dd67bd9a49343
isyankarcoder <gencvolkan@gmail.com>
2021-02-09 14:13:31 +0300

    operator

--- Pointer/pointer.c
+++ Pointer/pointer.c
@@ -19,5 +19,12 @@ int main(){
 
     *pc = 55;
      printf("Adress of c: %p\n", &c);
-    printf("Value of c: %d\n\n ", c);
+     printf("Value of c: %d\n\n ", c);
+
+     int a = 5;
+     int b = 5;
+
+     printf("Value of a: %d\n", a++);
+     printf("Value of b: %d\n", ++b);
+     printf("Value of a: %d\n", a);
 }
